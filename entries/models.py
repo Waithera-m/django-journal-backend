303881079b768fd39379c99dc6e112df2345b118
Waithera <njihiamary11@gmail.com>
2025-03-26 22:20:12 +0300

    Create initial db schema

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,104 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(upload_to='profile_pics/')
+    # objects = models.Manager()
+    # active_objects = models.Manager()
+
+    def save_profile(self):
+        """
+        method saves added profile
+        """
+        self.save()
+    
+    def update_profile(self, using=None, fields=None, **kwargs):
+        """
+        class method updates profile properties
+        """
+        if fields is not None:
+            fields = set(fields)
+            deferred_fields = self.get_deferred_fields()
+
+            if fields.intersection(deferred_fields):
+                fields = fields.union(deferred_fields)
+                
+        super().refresh_from_db(using, fields, **kwargs)
+
+    def delete_profile(self):
+        """
+        method deletes saved profile
+        """
+        self.delete()
+    
+    def __str__(self):
+        return f'{self.user.username}'
+           
+class JournalEntry(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_archived = models.BooleanField(default=False)
+    tags = models.ManyToManyField('Tag', related_name='entries_tags')
+    objects = models.Manager()
+    active_objects = models.Manager()
+
+    def save_entry(self):
+        """
+        method saves added journal entry
+        """
+        self.save()
+    
+    def update_entry(self, using=None, fields=None, **kwargs):
+        """
+        method updates saved journal entry
+        """
+        if fields is not None:
+            fields = set(fields)
+            deferred_fields = self.get_deferred_fields()
+
+            if fields.intersection(deferred_fields):
+                fields = fields.union(deferred_fields)
+        super().refresh_from_db(using, fields, **kwargs)
+    
+    def delete_entry(self):
+        """
+        method deletes saved journal entry
+        """
+        self.delete()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def save_tags(self):
+        """
+        method saves added tag
+        """
+        self.save()
+    
+    def update_tags(self, using=None, fields=None, **kwargs):
+        """
+        class method updates profile properties
+        """
+        if fields is not None:
+            fields = set(fields)
+            deferred_fields = self.get_deferred_fields()
+
+            if fields.intersection(deferred_fields):
+                fields = fields.union(deferred_fields)
+                
+        super().refresh_from_db(using, fields, **kwargs)
+
+    def delete_tags(self):
+        """
+        method deletes saved tag
+        """
+        self.delete()
+    
+    def __str__(self):
+        return f'{self.name}'
--- entries/tests.py
+++ entries/tests.py
@@ -1,3 +1,154 @@
 from django.test import TestCase
+from .models import Profile, JournalEntry, Tag
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileClassTests(TestCase):
+    '''
+    class tests the Profile class model
+    '''
+    def tearDown(self):
+        """
+        method deletes all objects created during the tests
+        """
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+
+    def setUp(self):
+        self.user = User(email='test@gmail.com', username='test', password='test', first_name='test', last_name='test')
+        self.user.save()
+        self.profile = Profile(user=self.user, bio='test bio', profile_pic='test.jpg')
+
+    def test_instance(self):
+        """
+        method checks if a profile object is initialized properly
+        """
+        self.assertIsInstance(self.profile, Profile)
+
+    def test_save_profile(self):
+        """
+        method checks if a profile object is saved
+        """
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_profile_update(self):
+        """
+        method checks if a profile object is updated
+        """
+        profile = Profile.objects.create(user=self.user, bio='test bio 1', profile_pic='test.jpg')
+        Profile.objects.filter(pk=profile.pk).update(bio='test bio 2')
+        profile.update_profile()
+        self.assertEqual(profile.bio, 'test bio 2')
+    
+    def test_delete_profile(self):
+        """
+        method checks if a profile object is deleted
+        """
+        profile = Profile.objects.create(user=self.user, bio='test bio to delete', profile_pic='test.jpg')
+        Profile.objects.filter(pk=profile.pk).delete()
+        profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+        
+class TagClassTests(TestCase):
+
+    '''
+    class tests the Tag class model
+    '''
+
+    def tearDown(self):
+        """
+        method deletes all objects created during the tests
+        """
+        Tag.objects.all().delete()
+
+    def setUp(self):
+        self.tag = Tag(name='test tag')
+        
+    def test_instance(self):
+        """
+        methods checks if tag model is initialized properly
+        """
+        self.assertIsInstance(self.tag, Tag)
+
+    def test_save_tag(self):
+        """
+        methods checks if tag model is saved
+        """
+        self.tag.save_tags()
+        tags = Tag.objects.all()
+        self.assertTrue(len(tags) > 0)
+    
+    def test_update_tag(self):
+        """
+        methods checks if tag model is updated
+        """
+        tag = Tag.objects.create(name='test tag to update')
+        Tag.objects.filter(pk=tag.pk).update(name='test tag updated')
+        tag.update_tags()
+        self.assertEqual(tag.name, 'test tag updated')
+    
+    def test_delete_tag(self):
+        """
+        methods checks if tag model is deleted
+        """
+        tag = Tag.objects.create(name='test tag to delete')
+        Tag.objects.filter(pk=tag.pk).delete()
+        tag.delete_tags()
+        tags = Tag.objects.all()
+        self.assertTrue(len(tags) == 0)
+
+class JournalEntryClassTests(TestCase):
+    """
+    class tests the JournalEntry class model
+    """
+
+    def tearDown(self):
+        """
+        method deletes all objects created during the tests
+        """
+        Profile.objects.all().delete()
+        Tag.objects.all().delete()
+        JournalEntry.objects.all().delete()
+
+    def setUp(self):
+        self.user = User.objects.create(email="test@gmail.com", username= "Doeish", password= "test", first_name= "John", last_name= "Doe")
+        self.profile = Profile.objects.create(user=self.user, bio="test bio", profile_pic="test.jpg")
+        self.tag = Tag.objects.create(name="test tag")
+        self.entry = JournalEntry.objects.create(author=self.profile, title="test title", body="test body", is_archived=False)
+        self.entry.tags.add(self.tag)
+    
+    def test_instance(self):
+        """
+        method checks if a journal entry object is initialized properly
+        """
+        self.assertIsInstance(self.entry, JournalEntry)
+    
+    def test_save_entry(self):
+        """
+        method checks if a journal entry object is saved
+        """
+        self.entry.save_entry()
+        entries = JournalEntry.objects.all()
+        self.assertTrue(len(entries) > 0)
+    
+    def test_update_entry(self):
+        """
+        method checks if a journal entry object is updated
+        """
+        entry = JournalEntry.objects.create(author=self.profile, title="test title", body="test body", is_archived=False)
+        JournalEntry.objects.filter(pk=entry.pk).update(title="test title updated")
+        entry.update_entry()
+        self.assertEqual(entry.title, "test title updated")
+    
+    def test_delete_entry(self):
+        """
+        method checks if a journal entry object is deleted
+        """
+        self.entry.save_entry()
+        self.entry.delete_entry()
+        entries = JournalEntry.objects.all()
+        print(entries)
+        self.assertTrue(len(entries) == 0)
